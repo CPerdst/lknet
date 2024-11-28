d876a226f2fce081d4499d3ea380a27dce2cdc20
xefgiyvucn <212086130@qq.com>
2024-11-28 12:22:25 +0800

    update: use lklog submodule

--- src/source/logger/appender.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by l1Akr on 24-8-10.
-//
-
-#include "appender.h"
-
-namespace logger {
-} // logger
--- src/source/logger/packer.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by l1Akr on 24-8-11.
-//
-
-#include "packer.h"
-
-const char* packer::RootFormatStr               = "[%level] [%s {%Y-%m-%d %H:%M:%S}] %filepath:%line (%func): %message\n";
-const packer::level packer::RootLevel           = packer::level::Debug;
