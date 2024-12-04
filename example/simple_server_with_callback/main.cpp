5b58f7fd184af69535b758bb67ee832ed2c1c447
l1Akr <2539439359@qq.com>
2024-12-04 20:43:10 +0800

    更新：添加回调样例

--- example/simple_server_with_callback/main.cpp
+++ example/simple_server_with_callback/main.cpp
@@ -1,3 +1,48 @@
 //
 // Created by zwj1 on 24-12-4.
 //
+
+#include "iostream"
+#include "Server.h"
+
+void initLogger() {
+    logger::logger::Root()->setLevel(packer::Debug);
+    logger::logger::Root()->setLogFormater("[%level] [%s {%Y-%m-%d %H-%M-%S}]: %message\n");
+}
+
+int main(int argc, char* argv[]) {
+    initLogger();
+    // connect to 127.0.0.1 : 8080
+    std::string host;
+    unsigned short port = 0;
+    if(argc == 1){
+        host = "127.0.0.1";
+        port = 8080;
+    }else if(argc == 2){
+        host = argv[1];
+        port = 8080;
+    }else if(argc == 3){
+        host = argv[1];
+        port = std::stoi(argv[2]);
+        if(!port){
+            RootError() << "argument 'port' parse failed.";
+            exit(EXIT_FAILURE);
+        }
+    }else{
+        RootError() << "usage: " << argv[0] << " [host] [port]";
+    }
+
+    Server s(host, port);
+    std::function<void(Message)> handler = [](Message message){
+        // handle message
+        // ...
+        RootInfo() << "handled message.";
+        // output handled message
+        std::string s_msg = message.parseMessageToString();
+        RootInfo() << s_msg;
+    };
+    s.setMessageHandler(handler);
+    s.start();
+
+    return 0;
+}
